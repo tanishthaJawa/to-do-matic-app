f9962dfcf3b063431d0d62960ce3feccce237af5
tanishthaJawa <tanishtha.jawa@outlook.com>
2022-07-26 20:08:17 +0530

    initial commit

---
+++ src/components/UsePrevious.js
@@ -0,0 +1,10 @@
+import { useRef, useEffect } from "react";
+
+function UsePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
+export default UsePrevious;  
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Walk my Dog", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
